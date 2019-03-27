504a9053d98465c6f8014ca1ebe4364dca3f43f9
Weikai Wang <wc563388548@gmail.com>
2019-03-27 18:21:49 +0900

    set up fake data(wip)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Mock data
+const items = [
+  {
+    "id": 1,
+    "title": "去日本旅游",
+    "price": 200,
+    "date": "2019-04-01",
+    "category": {
+      "id": "1",
+      "name": "旅行",
+      "type": "outcome",
+    }
+  },
+];
 
 class App extends Component {
   render() {
