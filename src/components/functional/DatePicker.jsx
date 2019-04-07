47939d04a0ef225a2059bf190844d251c3d24c9e
WWK563388548 <wc563388548@gmail.com>
2019-04-07 14:02:56 +0900

    complete dropdown menu

--- src/components/functional/DatePicker.jsx
+++ src/components/functional/DatePicker.jsx
@@ -10,6 +10,13 @@ class DatePicker extends Component {
         };
     }
 
+    range = (size, startAt) => {
+        const arr = [];
+        for (let i = 0; i < size; i++){
+            arr[i] = startAt + i
+        }
+        return arr;
+    }
     toggleDropdown = (event) => {
         event.preventDefault();
         this.setState({
@@ -17,9 +24,16 @@ class DatePicker extends Component {
         });
     }
 
+    padLeft = (num) => {
+        return num < 10 ? '0' + num : num;
+    } 
+
     render() {
         const {year, month} = this.props;
         const {isOpen} = this.state;
+        const monthRange = this.range(12, 1);
+        // console.log(monthRange);
+        const yearRange = this.range(12, -4).map(item => item + Number(year));
 
         return (
             <div className="dropdown date-picker-component">
@@ -32,7 +46,22 @@ class DatePicker extends Component {
                 </button>
                 {isOpen && 
                     <div className="dropdown-menu" style={{display: "block"}}>
-                        <h1>Menu</h1>
+                        <div className="row">
+                            <div className="col border-right">
+                                {yearRange.map((yearNumber, index) => 
+                                    <a key={index} className="dropdown-item">
+                                            {yearNumber} 年
+                                    </a>
+                                )}
+                            </div>
+                            <div className="col">
+                                {monthRange.map((monthNumber, index) => 
+                                    <a key={index} className="dropdown-item">
+                                        {this.padLeft(monthNumber)} 月
+                                    </a>
+                                )}
+                            </div>
+                        </div>
                     </div>
                 }
             </div>
