8060c60712da9892b2c2c938b6b335fcc483c910
WWK563388548 <wc563388548@gmail.com>
2019-07-15 23:44:35 +0900

    add route for every component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
+import Create from './components/Create/Create'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-      <Fragment>
-        <Home />
-      </Fragment>
+      <Router>
+        <Fragment>
+          <Route path="/" exact component={Home} />
+          <Route path="/create" component={Create} />
+        </Fragment>
+      </Router>
     );
   }
 }
---
+++ src/components/Create/Create.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Create  = () => {
+    return (<h1>This is create page</h1>);
+}
+
+export default Create;
