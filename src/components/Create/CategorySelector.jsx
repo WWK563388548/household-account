45e7357d77074764ab4c5100bbe8f37ce350d955
WWK563388548 <wc563388548@gmail.com>
2019-07-21 21:47:56 +0900

    finsh CategorySelector and it's test

--- src/components/Create/CategorySelector.jsx
+++ src/components/Create/CategorySelector.jsx
@@ -3,15 +3,41 @@ import Ionicon from 'react-ionicons';
 import PropTypes from 'prop-types';
 
 class CategorySelector extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedCategoryId: props.selectedCategory && props.selectedCategory.id,
+        };
+    }
+
+    selectCategory = (evt, category) => {
+        this.setState({
+            selectedCategoryId: category.id,
+        });
+        this.props.onSelectCategory(category);
+        // 阻止默认行为
+        evt.preventDefault();
+    }
+
     render() {
         const { categories } = this.props;
+        const { selectedCategoryId } = this.state;
+
         return (
             <div className="category-select-component">
                 <div className="row">
                     {
                         categories.map((category, index) => {
+                            const activeClassName = (selectedCategoryId === category.id) 
+                                ? 'category-item col-3 active' : 'category-item col-3';
                             return (
-                                <div key={index} className="category-item">
+                                <div 
+                                    key={index} 
+                                    onClick={(evt) => this.selectCategory(evt, category)}
+                                    className={activeClassName}
+                                >
                                     <Ionicon 
                                         className="rounded-circle"
                                         fontSize="50px"
--- src/components/Create/__test__/CategorySelector.test.js
+++ src/components/Create/__test__/CategorySelector.test.js
@@ -29,14 +29,37 @@ let props = {
     onSelectCategory: jest.fn(),
 };
 
+let props_with_category = {
+    categories,
+    onSelectCategory: jest.fn(),
+    selectedCategory: categories[0],
+};
+
 describe('Test CategorySelect componet', () => {
     it('renders with categories should render the correct items', () => {
         const wrapper = mount(<CategorySelector {...props} />);
         expect(wrapper.find('.category-item').length).toEqual(categories.length);
         expect(wrapper.find('.category-item.active').length).toEqual(0);
-        
+
         const firstIcon = wrapper.find('.category-item').first().find(Ionicon);
         expect(firstIcon.length).toEqual(1);
         expect(firstIcon.props().icon).toEqual(categories[0].iconName);
+    });
+
+    it('renders selectCategory with category item with highlight', () => {
+        const wrapper = mount(<CategorySelector {...props_with_category} />);
+        // 第一个类别项目应该携带active类名
+        expect(wrapper.find('.category-item').first().hasClass('active')).toEqual(true);
+    });
+
+    it('click the item should add active class and trigger the callback', () => {
+        const wrapper = mount(<CategorySelector {...props_with_category} />);
+        // 点击第二个类别项目
+        wrapper.find('.category-item').at(1).simulate('click');
+        // 第二个类别项目添加active类名
+        expect(wrapper.find('.category-item').at(1).hasClass('active')).toEqual(true);
+        // 第一个类别项目添加应该移除active类名
+        expect(wrapper.find('.category-item').first().hasClass('active')).toEqual(false);
+        expect(props_with_category.onSelectCategory).toHaveBeenCalledWith(categories[1]);
     });
 });
